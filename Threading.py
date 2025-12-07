34ea96f8481b4d74fa4d2573438ac1038204da3a
ahmed <ahmed23012555@gmail.comgit config --global user.email ahmed23012555@gmail.com>
2025-12-07 15:06:11 +0000

    Initial commit

---
+++ Threading.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import threading
+import time
+
+
+# In[6]:
+
+
+def print_1():
+    print("starting of a thread:",threading.current_thread().name)
+    time.sleep(0.02)
+    print("Finishing of a thread:",threading.current_thread().name)
+    
+def print_2():
+    print("starting of a thread:",threading.current_thread().name)
+    print("Finishing of a thread:",threading.current_thread().name)
+a = threading.Thread(target=print_1,name="Thread-1")
+b = threading.Thread(target=print_2,name="Thread-2")
+a.start()
+b.start()
+
+
+# In[ ]:
+
+
+
+
